1b3b54b562804dcfe875f1454618e26eca69e577
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-28 19:52:25 -0500

    Improve system block styles.

--- foundation.js
+++ foundation.js
@@ -1,6 +1,46 @@
 dg.modules.foundation = new dg.Module();
 
 /**
+ * Implements hook_regions_build_alter().
+ */
+function foundation_blocks_build_alter(blocks) {
+  if (blocks.admin_menu) {
+    blocks.admin_menu._attributes['class'].push('medium-6', 'columns');
+  }
+  if (blocks.powered_by) {
+    blocks.powered_by._attributes['class'].push('medium-6', 'columns');
+  }
+}
+
+/**
+ * Implements hook_block_view_alter().
+ */
+function foundation_block_view_alter(element, block) {
+
+  switch (block.get('id')) {
+
+    // Make the main menu into a foundation menu.
+    case 'main_menu':
+      element.menu._attributes['class'].push('menu');
+      break;
+
+    // Make the main menu into a foundation menu.
+    case 'admin_menu':
+      element.menu._attributes['class'].push('menu');
+      break;
+
+    // Make the powered by block into a foundation menu.
+    case 'powered_by':
+      element.list._items.push(dg.l('Foundation', 'http://foundation.zurb.com'));
+      element.list._items.reverse();
+      element.list._attributes['class'].push('menu', 'align-right');
+      break;
+
+  }
+
+}
+
+/**
  * Implements hook_form_alter().
  */
 function foundation_form_alter(form, form_state, form_id) {
